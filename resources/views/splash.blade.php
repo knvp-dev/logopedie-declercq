fc2d25efa052f858a48d70c8462cae357c4539c7
kenny vanpaemel <kennyvanpaemel@MacBook-Air-van-kenny.local>
2016-08-08 18:05:54 +0200

    create splash page

---
+++ resources/views/splash.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Logopedie Declercq</title>
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700,500' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+<body>
+
+<div class="container">
+
+<img src="{{ asset('images/logo.svg') }}" alt="" class="logo">
+<hr>
+<div class="clearfix"></div>
+
+<div class="info">
+
+	<h3>Contactgegevens</h3>
+
+	<ul>
+		<li>Klaroenstraat 9</li>
+		<li>8540 Deerlijk</li>
+		<li>info@logopedie-declercq.be</li>
+		<li>0474 78 42 65</li>
+	</ul>
+</div>
+
+<div class="photos col-md-12">
+
+	<img src="images/praktijk/01.jpg" alt="" class="col-md-4">
+	<img src="images/praktijk/02.jpg" alt="" class="col-md-4">
+	<img src="images/praktijk/03.jpg" alt="" class="col-md-4">
+	
+</div>
+
+</div>
+	
+</body>
+</html>
